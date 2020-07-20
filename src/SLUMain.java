8935f2796db5c228cc84df1b472a4cadff5bcb98
benjaminrayharris <benjaminrayharris@gmail.com>
2020-07-20 13:10:29 -0700

    testing and finalizing files for first release

--- src/SLUMain.java
+++ src/SLUMain.java
@@ -1,160 +1,139 @@
 // Author: Benjamin Harris
-// Class for the SpeciesList Updater program
+// main class for the SpeciesListUpdater program
 // July, 11 2020
 
-//import java.awt.Dimension;
-//import javax.swing.JFrame;
-//import java.awt.event.WindowAdapter;
-//import java.awt.event.WindowEvent;
-//import java.io.IOException;
-//import java.io.FileNotFoundException;
-//import java.io.*;
-//Class for the SpeciesList Updater program
-//July, 11 2020
-
-//import java.awt.Dimension;
-//import javax.swing.JFrame;
-//import java.awt.event.WindowAdapter;
-//import java.awt.event.WindowEvent;
-//import java.io.IOException;
-//import java.io.FileNotFoundException;
-//import java.io.*;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URI;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.EOFException;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedInputStream;
+import java.nio.file.Path;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.io.EOFException;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.BufferedWriter;
-import java.io.DataInputStream;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.InputStream;
-import javax.swing.JFileChooser;
+import java.net.URL;
+import java.net.URI;
+import java.net.MalformedURLException;
+import java.awt.Desktop;
+import java.security.MessageDigest;
 import javax.swing.JOptionPane;
-import javax.swing.ImageIcon;
-import org.apache.commons.io.FileUtils;
 
 public class SLUMain {
 	public static void main(String[] args) {
-		
-		/*
-		JFrame win = new JFrame("Species List Manager");
-		String exitQ = "Are you sure you want to exit?";
-		win.setMinimumSize(new Dimension(800, 600));
-		//win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		win.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-		win.addWindowListener(
-			new WindowAdapter() {
-				@Override public void windowClosing(WindowEvent e) { 
-					boolean result = UtilityShed.ask(exitQ);
-					if (result) System.exit(0);
-				}
-			}
-		);
-		win.getContentPane();
-		win.pack();
-		win.setVisible(true);
-		win.setLocationRelativeTo(null);
-		//win.setIconImage(eng.sbsImage);
-		*/
-
-		checkVersion();
-		popup("in main() about to set variables", null);
-		//String url = "https://github.com/benjaminrayharris/SpeciesList/blob/master/sl.jar";
-		//String url = "https://github.com/benjaminrayharris/SpeciesList/raw/version_2_0/test.txt";
-		String url = "";
-		
+		String url = "https://github.com/benjaminrayharris/SpeciesList/raw/master/sl.jar";
 		String dir = getDir();
-		
-		//String file = dir + File.separator + "sl.jar";
-		//String bupf = dir + File.separator + "bu.sl.jar";
-		//String tmpf = dir + File.separator + "temp.sl.jar";
-		String file = dir + File.separator + "test.txt";
-		String bupf = dir + File.separator + "bu.test.txt";
-		String tmpf = dir + File.separator + "temp.test.txt";
-		
-		
-		popup("in main() verify file is there", null);
-		//verify file is available to download
-
-		if (ask("\n    Should current version be backed up?           \n"
-				+ "\n    And overwrite the previously backed up version?")) {
-			backupFile(file, bupf);
+		String file = dir + File.separator + "sl.jar";
+		String bupf = dir + File.separator + "bu.sl.jar";
+		String tmpf = dir + File.separator + "temp.sl.jar";
+		if (ask("\n\nLast chance to back out. Abort upgrade?\n\n'Yes' to exit\n\n'No' to continue with upgrade\n\n"))
+			System.exit(0);
+		getFileFromURL(url, tmpf);
+		if (!compareChecksums(tmpf)) {
+			pop("ERROR: Downloaded file is corrupt.\n\nAborting upgrade process...");
+			System.exit(0);
 		} // end if
-		
-		//download new version
-		getFileFromURL(url, file, tmpf);
-		
-		//start new version
-		
-		
+		backup(file, bupf);
+		renameFile(tmpf, file);
+		try {			
+			Desktop.getDesktop().open(new File(file));
+		} catch (Exception e) {
+			pop("openUpdater(): " + e);
+		} // end try-catch
 	} // end main()
-	
-	private static void getFileFromURL (String url, String file, String tmpf) {
-		
-		    try {
-		    	InputStream in = URI.create(url).toURL().openStream();
-		        //Files.copy(in, file);
-		    	Files.copy(in, Paths.get(file));
-		    } catch (Exception e) {
-		    	
-		    } // end try-catch
-		
-	} // end getFileFromURL()
-	
-	private static String jget (String url) {
 
-		URL u;
-		InputStream is = null;
-		BufferedInputStream bis = null;
-		DataInputStream dis;
-		String s = null;
+	private static void pop (String msg) {
+		JOptionPane.showMessageDialog(null, msg, "Alert!", JOptionPane.INFORMATION_MESSAGE, null);
+	} // end pop()
+
+	private static boolean ask (String qstn) {
+		int opt = JOptionPane.showConfirmDialog(null, qstn, "???????", JOptionPane.YES_NO_OPTION);
+		return (opt == JOptionPane.YES_OPTION);
+	} // end ask()
+
+	private static int askif (String qstn) {
+		return JOptionPane.showConfirmDialog(null, qstn);
+	} // end askif()
+
+	private static String getDir () {
+		File f = null;
+		String dirPath = null;
 		try {
-			u = new URL(url);
-			is = u.openStream();
-			bis = new BufferedInputStream(is);
-			dis = new DataInputStream(bis);
-			while ((s = dis.readUTF()) != null) {
-				popup(s, null);
-			}  // end while
-		} catch (MalformedURLException mue) {
-			System.err.println("Ouch - a MalformedURLException happened.");
-			mue.printStackTrace();
-			System.exit(2);
-		} catch (IOException ioe) {
-			System.err.println("Oops- an IOException happened.");
-			ioe.printStackTrace();
-			System.exit(3);
-		} finally {
-			try {
-				is.close();
-			} catch (IOException ioe) {
-    		
-			} // end try-catch
-		} // end try-catch-catch-finally
-		return s;
+			f = new File(SLUMain.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getParentFile();
+			dirPath = f.toString();
+		} catch (Exception e) {
+			pop("setDir(): " + e);
+		} // end try-catch
+		return dirPath;
+	} // end getDir()
 
-		
-	} // end jget()
+	private static void getFileFromURL (String url, String file) {
+		try {
+			Files.deleteIfExists(Paths.get(file));
+			InputStream in = URI.create(url).toURL().openStream();
+			Files.copy(in, Paths.get(file));
+			File f = new File(file);
+			f.setExecutable(true);
+		} catch (Exception e) {
+		} // end try-catch
+	} // end getFileFromURL()
+
+	private static boolean compareChecksums (String path) {
+		boolean isSame = false;
+		try {
+			String actual = getActualChecksum(path);
+			String known = getKnownChecksum();
+			pop(actual + "\n" + known);
+			return actual.equals(known);
+		} catch (Exception e) {
+		} // end try-catch
+		return isSame;
+	} // end compareChecksums()
+
+	private static String getActualChecksum (String path) throws IOException {
+		String str = "";
+		try {
+			File file = new File(path);
+			MessageDigest digest = MessageDigest.getInstance("MD5");
+			//Get file input stream for reading the file content
+			FileInputStream fis = new FileInputStream(file);
+
+			//Create byte array to read data in chunks
+			byte[] byteArray = new byte[1024];
+			int bytesCount = 0;
+
+			//Read file data and update in message digest
+			while ((bytesCount = fis.read(byteArray)) != -1) {
+				digest.update(byteArray, 0, bytesCount);
+			} // end while
+
+			//close the stream; We don't need it now.
+			fis.close();
+
+			//Get the hash's bytes
+			byte[] bytes = digest.digest();
+			//This bytes[] has bytes in decimal format;
+			//Convert it to hexadecimal format
+			StringBuilder sb = new StringBuilder();
+			for(int i=0; i< bytes.length ;i++) {
+				sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+			} // end for
+
+			//return complete hash
+			str = sb.toString();
+		} catch (Exception e) {
 	
+		} // end try-catch
+		return str;
+	} // end getActualChecksum()
 
-	private static String checkVersion () {
-		String url = "https://github.com/benjaminrayharris/SpeciesList/raw/version_2_0/test.txt";
+	private static String getKnownChecksum () {
+		String url = "https://github.com/benjaminrayharris/SpeciesList/raw/master/sl.jar.checksum";
 		URL u;
 		InputStream is = null;
 		BufferedInputStream bis = null;
 		BufferedReader d;
-		DataInputStream dis;
 		String s = null;
 		String str = null;
 		try {
@@ -163,216 +142,70 @@ public class SLUMain {
 			bis = new BufferedInputStream(is);
 			d = new BufferedReader(new InputStreamReader(bis));
 			while ((s = d.readLine()) != null) {
-				popup(s, null);
 				str = s;
 			}  // end while
 		} catch (EOFException eofe) {
-			popup("in catch: " + s, null);
+			pop("checkVersion(): " + eofe);
 		} catch (MalformedURLException mue) {
-			System.err.println("Ouch - a MalformedURLException happened.");
-			mue.printStackTrace();
-			System.exit(2);
+			pop("checkVersion(): " + mue);
 		} catch (IOException ioe) {
-			System.err.println("Oops- an IOException happened.");
-			ioe.printStackTrace();
-			System.exit(3);
+			pop("checkVersion(): " + ioe);
 		} finally {
 			try {
 				is.close();
 			} catch (IOException ioe) {
 			} // end try-catch
 		} // end try-catch-catch-finally
-		popup(str, null);
 		return str;		
-	} // end checkVersion()
-	
-	
-	
-	private static String getDir () {
-		File f = null;
-		String dirPath = null;
-		try {
-			f = new File(SLUMain.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getParentFile();
-			dirPath = f.toString();
-		} catch (Exception e) {
-			popup("setDir(): " + e, null);
-		} // end try-catch
-		return dirPath;
-	} // end setDir()
-	
-	private static void getFileFromInternet (String url, String path, String tmpf) {
-		URL u;
-		//InputStream is = null;
-		//BufferedInputStream bis = null;
-		//DataInputStream dis = null;
-		//String s = null;
-			
-		File q;
+	} // end getKnownChecksum()
 
-
-
-        popup(tmpf, null);
-		try {
-
-			popup(url, null);
-			u = new URL(url);
-			
-			popup(u + "", null);
-			
-			q = new File(u.toURI());
-			//is = u.openStream();
-			
-
-	        try  {
-	           
-	        
-	        	InputStream is = new FileInputStream(tmpf);
-	        	OutputStream os = new FileOutputStream(path);
-	 
-	            long fileSize = q.length();
-	 
-	            byte[] allBytes = new byte[(int) fileSize];
-	 
-	            is.read(allBytes);
-	 
-	            os.write(allBytes);
-	            
-	            is.close();
-	            os.close();
-	 
-	        } catch (IOException ex) {
-	            ex.printStackTrace();
-	        }
-			
-			
-			//URL resource = new URL(url);
-			//File temp = new File(tmpf);
-			//File file = new File(path);
-			//FileUtils.copyURLToFile(resource, temp);
-		
-			//String str = readFile(tmpf, false);
-			
-			//popup(str, null);
-			
-			
-			//String filename = "";
-			//Response r = Jsoup.connect(url)
-		    //.followRedirects(true) // follow redirects (it's the default)
-			//    .ignoreContentType(true) // accept not just HTML
-			//    .maxBodySize(10*1000*1000) // accept 10M bytes (default is 1M), or set to 0 for unlimited
-			//    .execute(); // send GET request
-			//FileOutputStream out = new FileOutputStream(new File(filename));
-			//out.write(r.bodyAsBytes());
-			//out.close();
-			
-		} catch (Exception e) {
-			popup("getFileFromInternet(): " + e, null);
-		} // end try-catch-catch
-	} // end getFileFromInternet()
-	
-	
-	private static void popup (String msg, ImageIcon icon) {
-		JOptionPane.showMessageDialog(null, msg, "Alert!", JOptionPane.INFORMATION_MESSAGE, icon);
-	} // end popup()
-	
-	private static boolean ask (String qstn) {
-		int opt = JOptionPane.showConfirmDialog(null, qstn, "???????", JOptionPane.YES_NO_OPTION);
-		return (opt == JOptionPane.YES_OPTION);
-	} // end ask()
-	
-	private static void backupFile (String file, String bupf) {
-		popup("backing up old sl.jar", null);
-	} // end backupFile()
-	
-	private static String getin (String prmpt) {
-		return JOptionPane.showInputDialog(prmpt);
-	} // end getin()
-
-	private static InputStream loadFile (String path) {
-		InputStream in = SLUMain.class.getResourceAsStream(path);
-		if (in == null) in = SLUMain.class.getResourceAsStream("/" + path);
-		return in;
-	} // end loadFile()
-	
-	// readFile() return value is non-null for success, null for failure
-	private static String readFile (String path, boolean chooser) {
-		String strToParse = "";
-		File file;
-		try {
-			if (!chooser) {
-				if (path == null) {
-					popup("debug readFile(): 'path' can't be null", null); //////////////////////////
-					return null;
-				} // end if
-				file = new File(path);
+	private static void backup (String file, String bupf) {
+		String prmpt;
+		int result;
+		int yes = JOptionPane.YES_OPTION;
+		int no = JOptionPane.NO_OPTION;
+		prmpt = "\n\nBack up current version and overwrite the previously backed up version?";
+		prmpt += "\n\n'Yes' to overwrite previous backup.";
+		prmpt += "\n\n'No' to overwrite current program.";
+		prmpt += "\n\n'Cancel' to abort upgrade process.\n\n";
+		result = askif(prmpt);
+		if (result == yes) {
+			prmpt = "\n\nThis will overwrite the previous (backed up) version of the program!";
+			prmpt += "\n\nLast chance to reconsider.";
+			prmpt += "\n\nDo you want to abort upgrade?";
+			prmpt += "\n\n'Yes' to cancel upgrade, and save BOTH the current version AND the backed up previous version";
+			prmpt += "\n\n'No' to continue with upgrade process, lose the previous version, back up the current version, and install the new version\n\n";
+			if (!ask(prmpt)) {
+				pop("!!!!!  after yes  !!!!!  result was no - continue with upgrade  !!!!!!!!!!");
+				renameFile(file, bupf);
 			} else {
-				JFileChooser fc = new JFileChooser((path == null ? "ezezez" : path));
-				fc.setAcceptAllFileFilterUsed(true);
-				int result = fc.showDialog(null, "Load File");
-				if (result == JFileChooser.APPROVE_OPTION) file = fc.getSelectedFile();
-				else {
-					popup("file load cancelled", null); /////////////////////////////////////////////////////////////////////////////
-					return null;
-				} // end if-else
-			} // end if
-			popup("Going to check if file exists.", null);
-			if (!file.exists()) {
-				popup(file.getName() + " does not exist", null);
-				return null;
-			} // end if
-			BufferedReader in = new BufferedReader( new FileReader(file));
-			String str = in.readLine();
-			strToParse = str;
-			str = in.readLine();
-			while (str != null) {
-				strToParse += "\n" + str;
-				str = in.readLine();
-			} // end while
-			in.close();
-		} catch (Exception e) { 
-			popup("debug readFile(): " + e, null);
-		} // end try-catch
-		return strToParse;
-	} // end readFile()
-
-	// saveFile() return value is null for success, string message for failure reason
- 	private static String saveFile (String st, String path, boolean chooser) {
- 		File file;
-		try {
-			if (!chooser) {
-				if (path == null) {
-					return "save was aborted - no file path"; ////////////////////
-				} // end if
-				file = new File(path);
-			} else {
-				JFileChooser fc = new JFileChooser((path == null ? "ezezez" : path));
-				fc.setAcceptAllFileFilterUsed(true);
-				int result = fc.showDialog(null, "Save File");
-				if ((result == JFileChooser.APPROVE_OPTION)) {
-					String di = fc.getSelectedFile().getParentFile().toString();
-					String name = fc.getSelectedFile().getName();
-					String ext = fc.getFileFilter().getDescription();
-					if (ext.equals("All Files")) ext = "";
-					else ext = ext.substring(1);
-					if (!name.endsWith(ext)) name = name + ext;
-					name = di + File.separator + name;
-					file = new File(name);
-					popup("File saved as:  " + name, null);
-				} else return "save was cancelled";
-			} // end if
-			if (file.getParentFile().exists()) {
-				PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(file)));
-				out.println(st);
-				out.close();
-			} else {
-				popup("debug saveFile(): file's parents doesn't exist", null);
-				return "problem with existence of file's 'parent'";
+				pop("!!!!!  after yes  !!!!!  result was yes - abort upgrade  !!!!!!!!!!!!!!!!!");
+				System.exit(0);
 			} // end if-else
-		} catch (Exception e) { 
-			popup("debug saveFile(): " + e, null);
-			return "problem in 'try': " + e;
+		} else if (result == no) {
+			prmpt = "\n\nThis will overwrite the current version of the program!";
+			prmpt += "\n\nDo you want to continue with upgrade?";
+			prmpt += "\n\n'Yes' to continue with upgrade process, lose the current version while preserving the previous version, and install the new version";
+			prmpt += "\n\n'No' to cancel upgrade, and save BOTH the current version AND the backed up previous version of program\n\n";
+			if (ask(prmpt)) {
+				pop("!!!!!  after no   !!!!!  result was yes - continue with upgrade  !!!!!!!!!");
+				//renameFile(file, bupf); 
+			} else {
+				pop("!!!!!  after no   !!!!!  result was no - abort upgrade  !!!!!!!!!!!!!!!!!!");
+				System.exit(0);
+			} // end if-else
+		} else System.exit(0);
+	} // end backup()
+
+	private static void renameFile (String sour, String dest) {
+		Path source = Paths.get(sour);
+		Path destination = Paths.get(dest);
+		try {
+			Files.deleteIfExists(destination);
+			Files.move(source, destination);
+		} catch (IOException e) {
+			pop("renameFile(): " + e);
 		} // end try-catch
-		return null;
-	} // end saveFile()
+	} // end renameFile()
 
 } // end class SLUMain
